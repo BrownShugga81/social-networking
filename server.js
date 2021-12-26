a954ac2044de7b22cbe018f7e4bf19520d3582bc
Chris Zuniga <zunigachristian81@gmail.com>
2021-12-26 16:35:43 -0600

    askbcs troubleshooting

--- server.js
+++ server.js
@@ -6,12 +6,13 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static('public'));
+
+app.use(require('./routes'));
+
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-networking');
 
 mongoose.set('debug', true);
 
-//app.use(require('./routes'));
 
 app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
